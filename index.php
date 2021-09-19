78d275d6ebadc08271a7e1cc7f78fec601c06fd4
Myrotvorets <support@myrotvorets.center>
2021-09-19 15:51:07 +0300

    Change the bootstrapping logic

--- index.php
+++ index.php
@@ -12,6 +12,15 @@
 use Myrotvorets\WordPress\Identigraf\Plugin;
 
 if ( defined( 'ABSPATH' ) ) {
-	require __DIR__ . '/vendor/autoload.php';
+	if ( defined( 'VENDOR_PATH' ) ) {
+		/** @psalm-suppress UnresolvableInclude, MixedOperand */
+		require constant( 'VENDOR_PATH' ) . '/vendor/autoload.php'; // NOSONAR
+	} elseif ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+		require __DIR__ . '/vendor/autoload.php';
+	} elseif ( file_exists( ABSPATH . 'vendor/autoload.php' ) ) {
+		/** @psalm-suppress UnresolvableInclude */
+		require ABSPATH . 'vendor/autoload.php';
+	}
+
 	Plugin::instance();
 }
