a70df2ca4198fde80eade240e02b939567a3db1a
Myrotvorets <support@myrotvorets.center>
2023-04-04 05:44:07 +0300

    Use a different dev container

--- .devcontainer/setup-wordpress.sh
+++ .devcontainer/setup-wordpress.sh
@@ -1,32 +1,13 @@
 #!/bin/sh
 
-DB_USER=wordpress
-DB_NAME=wordpress
-DB_PASS=wordpress
-DB_HOST=db
-DB_ROOT_PASS=wordpress
+/usr/local/bin/setup-wordpress.sh
 
-# See https://docs.github.com/en/codespaces/developing-in-codespaces/default-environment-variables-for-your-codespace
-if [ -n "${CODESPACE_NAME}" ] && [ -n "${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}" ]; then
-    WP_DOMAIN="${CODESPACE_NAME}-80.${GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN}"
+if [ -n "${RepositoryName}" ]; then
+    dir=/workspaces/${RepositoryName}
 else
-    WP_DOMAIN=localhost
+    dir=$(pwd)
 fi
 
-echo "Waiting for MySQL to come online..."
-second=0
-while ! mysqladmin ping -uroot -p"${DB_ROOT_PASS}" -h"${DB_HOST}" --silent && [ "${second}" -lt 60 ]; do
-    sleep 1
-    second=$((second+1))
-done
-if ! mysqladmin ping -uroot -p"${DB_ROOT_PASS}" -h"${DB_HOST}" --silent; then
-    echo "ERROR: mysql has failed to come online"
-    exit 1;
-fi
-
-if ! wp core is-installed 2> /dev/null; then
-    wp core install --url="http://${WP_DOMAIN}" --title="Development site" --admin_user=admin --admin_password=pass --admin_email=admin@localhost.localdomain --skip-email --locale=uk_UA
-    wp option set blog_public 0
-fi
-
+sudo rm -rf "/wp/wp-content/plugins/wp-identigraf"
+sudo ln -sf "${dir}" "/wp/wp-content/plugins/wp-identigraf"
 wp plugin activate wp-identigraf
